7859b8d227de24f5398a1cf509dc112f63463332
Siddhanta Gupta <siddhanta.sg@gmail.com>
2024-01-03 19:38:08 +0530

    feat: BoxPig now throws box towards player direction when he is in range add: ThrowableBox scene

--- Scenes/Characters/Mobs/Pigs/BoxPig/BoxPig.cs
+++ Scenes/Characters/Mobs/Pigs/BoxPig/BoxPig.cs
@@ -5,25 +5,30 @@ public partial class BoxPig : CharacterBody2D
 {
 	[Export]
 	public CharacterBody2D player;
+	public PackedScene throwableBoxScene;
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
+	public const int throwStrength = 100;
 	public bool isPlayerInRange = false;
 
 	public AnimationTree animationTree;
 	public AnimationNodeStateMachinePlayback animationPlayback;
 	public Sprite2D sprite;
+	public Marker2D boxMarker;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
 	public override void _Ready()
 	{
+		throwableBoxScene = ResourceLoader.Load<PackedScene>("res://Scenes/Items/Box/throwable_box.tscn");
 		sprite = GetNode<Sprite2D>("Sprite2D");
 		animationTree = GetNode<AnimationTree>("AnimationTree");
+		boxMarker = GetNode<Marker2D>("BoxMarker");
 		animationPlayback = animationTree.Get("parameters/playback").As<AnimationNodeStateMachinePlayback>();
+
 		animationTree.Active = true;
 		animationPlayback.Travel("Idle");
-
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -45,10 +50,13 @@ public partial class BoxPig : CharacterBody2D
 		if (player.Position.X < Position.X)
 		{
 			sprite.FlipH = false;
+			boxMarker.Position = new Vector2(-Math.Abs(boxMarker.Position.X), boxMarker.Position.Y);
+
 		}
 		else if (player.Position.X > Position.X)
 		{
 			sprite.FlipH = true;
+			boxMarker.Position = new Vector2(Math.Abs(boxMarker.Position.X), boxMarker.Position.Y);
 		}
 	}
 
@@ -74,8 +82,18 @@ public partial class BoxPig : CharacterBody2D
 		switch (anim_name)
 		{
 			case "Throw":
+				ThrowableBox throwableBox = throwableBoxScene.Instantiate<ThrowableBox>();
+				throwableBox.Position = boxMarker.Position;
+				AddChild(throwableBox);
+				// The LinearVelocity needs to be set after the call to AddChild
+				// because no GlobalPosition exists for box before it.
+				// both the below statement will give the same result.
+				throwableBox.LinearVelocity =
+					new Vector2(throwStrength, 0)
+					.Rotated(throwableBox.GlobalPosition.AngleToPoint(player.GlobalPosition));
+				// throwableBox.LinearVelocity = (player.GlobalPosition - throwableBox.GlobalPosition).Normalized() * throwStrength;
+
 				animationPlayback.Travel("Pick");
-				// Throw the box
 				break;
 
 			case "Pick":
@@ -91,10 +109,6 @@ public partial class BoxPig : CharacterBody2D
 
 			default:
 				break;
-		}
-		if (anim_name == "Pick")
-		{
-
 		}
 	}
 }
--- Scenes/Items/Box/ThrowableBox.cs
+++ Scenes/Items/Box/ThrowableBox.cs
@@ -1,15 +1,15 @@
 using Godot;
 using System;
 
-public partial class Box : RigidBody2D
+public partial class ThrowableBox : RigidBody2D
 {
 	private void OnBodyEntered(Node body)
 	{
 		if (body is Player)
 		{
 			// reduce player health
+			QueueFree();
 			GD.Print("Player collided");
 		}
-		QueueFree();
 	}
 }
